2eba63fddfd4c511274c1fe671301e8821f37975
Capril1042 <Caprilewis1042@gmail.com>
2018-10-24 22:29:36 -0400

    Adds Header

--- comic/src/App.js
+++ comic/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Characters from './components/Characters';
 import CharSelector from './components/CharSelector';
+import Header from './components/Header';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header/>
         <div className="Main-Body">
           <CharSelector/>
           <Characters/>
