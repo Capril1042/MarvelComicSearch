d6c15ee4ddd6ea824ce3c0eab6962e4c3d592d66
Capril1042 <Caprilewis1042@gmail.com>
2018-10-26 15:49:29 -0400

    Adds to Header

--- comic/src/components/Header.js
+++ comic/src/components/Header.js
@@ -3,8 +3,12 @@ import React from 'react';
 const Header= () => {
 return (
     <div className="Header">
-
+    <div className="title">
     Marvel Comic Search
+    </div>
+    <div className="info"> 
+    How it works
+    </div>
     </div>
 )
 }
