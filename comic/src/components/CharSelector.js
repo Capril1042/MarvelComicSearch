8edae43e977509fdd22b0d7e32b362729c47056e
Capril1042 <Caprilewis1042@gmail.com>
2018-10-30 22:31:24 -0400

    Layout of Character Selector

--- comic/src/components/CharSelector.js
+++ comic/src/components/CharSelector.js
@@ -4,7 +4,21 @@ const CharSelector = () => {
     
     return(
         <div className="CharSelector">
-            <p> Your choosen characters will apper here</p>
+            <p> choosen characters will apper here</p>
+           <div className="SelectorButtons">
+            <div className="SearchComics"> search comics button 
+            </div>
+            <div className="ClearCharacters"> reset button</div>
+            </div>
+            <div className="CharSelectorBox">
+                CharSelector
+            </div>
+            <div className="CharSelectorBox">
+                CharSelector
+            </div>
+            <div className="CharSelectorBox">
+                CharSelector
+            </div>  
         </div>
     );
 }
